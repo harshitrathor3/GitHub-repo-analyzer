0fc045dc1cca248e1c4707d1017c4087c85a5d83
Harshit Rathore <105155118+harshitrathor3@users.noreply.github.com>
2023-06-21 23:03:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ if __name__=='__main__':
     # text = 'https://github.com/cmooredev/RepoReader'
     # username = input('Enter the github username : ')
     st.title('GitHub Analyzer')
-    api_key = st.text_input('Enter your API key (this will be deleted when you close the application) : ')
+    # api_key = st.text_input('Enter your API key (this will be deleted when you close the application) : ')
     # st.secrets["openai_key"] = api_key
     username = st.text_input('Enter the github username : ')
     if username:
